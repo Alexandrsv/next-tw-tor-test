aeb449bea81000d81ec1e0aedd0646b75a0f26e0
Александр <zbc@mail.ru>
2022-08-10 16:36:37 +0300

    add duck-card

---
+++ components/main-page/MainPage.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+const MainPage = () => {
+  return (
+    <div className="min-h-screen w-screen grid place-content-center relative bg-black">
+      <div
+        className={
+          "grid border border-red-800 h-[500px] w-[320px] duck-bg select-text p-4 "
+        }
+      >
+        <div className={"grid  font-bold text-red-600 leading-[1.3rem]"}>
+          Ты слишком много выпил и потерял сознание. Ты просыпаешься в темной
+          камере, Голос рядом с тобой задает тебе вопрос, который меняет твою
+          жизнь
+        </div>
+        <div
+          className={
+            " self-end text-right font-bold text-xl text-red-600 leading-[1.3rem] pt-20 pb-2"
+          }
+        >
+          {" "}
+          <div
+            className={
+              "grid border border-red-600 p-2 bg-gradient-to-r from-transparent to-black"
+            }
+          >
+            Эй, приятель! Хочешь поговорить о утках?
+          </div>{" "}
+        </div>
+
+        <ol
+          className={
+            "self-end font-bold text-red-600 leading-[1.3rem] list-decimal ml-7"
+          }
+        >
+          <li>Поговорить о утках</li>
+          <li>Лежать спокойно</li>
+          <li>Позвать на помощь</li>
+          <li>Ударить по стене</li>
+        </ol>
+      </div>
+      <p></p>
+
+      {/*<div*/}
+      {/*  className={`bg-red-400 border border-blue-700 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-5 h-5 z-10`}*/}
+      {/*/>*/}
+      {/*<div*/}
+      {/*  className={` border border-blue-700 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-6 h-6 z-${12}`}*/}
+      {/*/>*/}
+      {/*<div*/}
+      {/*  className={` border border-blue-700 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-7 h-7 z-${12}`}*/}
+      {/*/>*/}
+      {/*<div*/}
+      {/*  className={` border border-blue-700 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-8 h-8 z-${12}`}*/}
+      {/*/>*/}
+      {/*<div*/}
+      {/*  className={`bg-blue-400 border border-blue-700 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-9 h-9 z-${0}`}*/}
+      {/*/>*/}
+    </div>
+  );
+};
+
+export default MainPage;
